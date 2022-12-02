1455b14104022bc909fd1631bb46043400594c96
Tom <ozammo13@gmail.com>
2022-12-02 11:18:41 +0000

    Committed the stuff that needed committing

--- PoseDetection.py
+++ PoseDetection.py
@@ -1,4 +1,4 @@
-mport cv2
+import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -6,7 +6,7 @@ mp_pose = mp.solutions.pose
 
 # For static images:
 IMAGE_FILES = []
-BG_COLOR = (192, 192, 192) # gray
+BG_COLOR = (0, 0, 255) # blue
 with mp_pose.Pose(
     static_image_mode=True,
     model_complexity=2,
